b4f55a4b7180362bdef42f572e2beecb67e9f535
marcosvmsa <marcosvmsa@gmail.com>
2022-07-05 14:01:31 -0700

    feat: database connection

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,25 @@
 const { Sequelize } = require('sequelize');
 
+const OPTIONS = {
+    dialect: 'postgres',
+    username: 'marcos',
+    password: 's3nh@',
+    port: 5432,
+    database: 'dev'
+}
 
-// Option 1: Passing a connection URI
-console.log(process.env.DB_CONNECTION_STRING)
-console.log('AUI')
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING)
-try {
+const sequelize = new Sequelize(OPTIONS)
+
+async function initializeDatabase() {
+  try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+}
+
+module.exports = {
+  initializeDatabase
+}
 
